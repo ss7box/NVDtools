033a60e282b775f73ef6be5b9c3a5dadbe4a7595
Michael J Mueller <michael.j.mueller@usps.gov>
2021-08-02 08:28:02 -0500

    add url for Tuxedo

--- checkNVD.py
+++ checkNVD.py
@@ -11,6 +11,9 @@ import requests
 
 #--- main ---
 
-url = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=openssl&pubStartDate=2021-07-17T00:00:00:000%20UTC-05:00"
+urlOpenssl = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=openssl&pubStartDate=2021-07-17T00:00:00:000%20UTC-05:00"
+urlTuxedo = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=tuxedo&pubStartDate=2021-02-10T00:00:00:000%20UTC-05:00"
+
+url = urlTuxedo
 r = requests.get(url, verify='/etc/ssl/certs').json()
 print(json.dumps(r, indent=4))
