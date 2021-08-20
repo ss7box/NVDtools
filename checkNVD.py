5c960edbbcee210a70c6759aeda6e2f1ef7881a3
mjm <ss7box@gmail.com>
2021-08-20 08:16:49 -0400

    structured query urls

--- checkNVD.py
+++ checkNVD.py
@@ -11,10 +11,19 @@ import requests
 
 #--- main ---
 
-urlOpenssl = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=openssl&pubStartDate=2021-07-17T00:00:00:000%20UTC-05:00"
-urlTuxedo = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=tuxedo&pubStartDate=2021-02-10T00:00:00:000%20UTC-05:00"
 
-url = urlTuxedo
-url = urlOpenssl
-r = requests.get(url, verify='/etc/ssl/certs').json()
+base = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword="
+date = "2021-08-15"
+startDate = "&pubStartDate=" + date +"T00:00:00:000%20UTC-05:00"
+
+#urlOpenssl = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=openssl&pubStartDate=2021-07-17T00:00:00:000%20UTC-05:00"
+#urlTuxedo = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=tuxedo&pubStartDate=2021-02-10T00:00:00:000%20UTC-05:00"
+
+urlTuxedo = base + "tuxedo" + startDate
+urlOpenssl = base + "openssl" + startDate
+
+r = requests.get(urlOpenssl, verify='/etc/ssl/certs').json()
+print(json.dumps(r, indent=4))
+
+r = requests.get(urlTuxedo, verify='/etc/ssl/certs').json()
 print(json.dumps(r, indent=4))
